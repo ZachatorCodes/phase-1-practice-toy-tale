549464841e396f59c98a2f7c853d72e078e385c5
Zach Schwartz <zachatorcodes@gmail.com>
2022-11-11 13:56:35 -0700

    Page now loads all toys from JSON server as cards

--- src/index.js
+++ src/index.js
@@ -13,3 +13,40 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+// Adds original data from JSON server after the DOM loads.
+document.addEventListener("DOMContentLoaded", () => {
+  const cardStorage = document.getElementById("toy-collection");
+  fetch("http://localhost:3000/toys")
+    .then(response => response.json())
+    .then(data => {
+      for (const obj of data) {
+        const cardDiv = document.createElement("div");
+        cardDiv.classList.add("card");
+        const h2 = document.createElement("h2");
+        const img = document.createElement("img");
+        const p = document.createElement("p");
+        const button = document.createElement("button");
+        for (const key in obj) {
+          if (key === "name") {
+            h2.textContent = obj[key];
+          }
+          else if (key === "image") {
+            img.src = obj[key];
+            img.classList.add("toy-avatar");
+          }
+          else if (key === "likes") {
+            p.textContent = `${obj[key]} Likes`;
+          }
+          button.classList.add("like-btn");
+          button.id = obj["id"];
+          button.textContent = "Like ❤️";
+        }
+        cardDiv.appendChild(h2);
+        cardDiv.appendChild(img);
+        cardDiv.appendChild(p);
+        cardDiv.appendChild(button);
+        cardStorage.appendChild(cardDiv);
+      }
+    });
+});
